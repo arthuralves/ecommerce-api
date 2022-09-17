c36ef2f2f6e5f961f0dd1b8a0d07a518cca82bc0
Arthur Alves <arthur.alves.rs@gmail.com>
2022-09-17 16:01:29 -0300

    added image to product model

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     sequence(:name) { |n| "Product #{n}" }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 100.0..400.0) }
+    image { Rack::Test::UploadedFile.new(Rails.root.join("spec/support/images/product_image.png")) }
 
 
     # Este recurso after :build permite que após a construção de um objeto de Product pela factory, a gente acesse ele e crie um model Game utilizando a factory e atribua em productable. Este productable é um recurso da associação polimórfica que tem objeto que está associado com o model utilizando os campos productable_type e productable_id    

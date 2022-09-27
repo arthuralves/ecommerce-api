a6a780be093b3467e6ca3bc6d7431a98bb74c1a2
Arthur Alves <arthur.alves.rs@gmail.com>
2022-09-27 17:51:28 -0300

    add POST methods for Coupon Controller

--- spec/requests/admin/v1/coupons/admin_request_spec.rb
+++ spec/requests/admin/v1/coupons/admin_request_spec.rb
@@ -18,4 +18,51 @@ RSpec.describe "Admin::V1::Coupons", type: :request do
     end
   end 
 
+  context "POST /coupons" do
+    let(:url) { "/admin/v1/coupons" }
+    
+    context "with valid params" do
+      let(:coupons) { { coupon: attributes_for(:coupon) }.to_json }
+
+      it 'adds a new Coupons' do
+        expect do
+          post url, headers: auth_header(user), params: coupons
+        end.to change(Coupon, :count).by(1)
+      end
+
+      it 'returns last added Coupons' do
+        post url, headers: auth_header(user), params: coupons
+        expected_coupon = Coupon.last.as_json(only: %i(id name code status discount_value max_use due_date))
+        expect(body_json['coupon']).to eq expected_coupon
+      end
+
+      it 'returns success status' do
+        post url, headers: auth_header(user), params: coupons
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context "with invalid params" do
+      let(:coupon_invalid_params) do 
+        { coupon: attributes_for(:coupon, name: nil) }.to_json
+      end
+
+      it 'does not add a new Coupons' do
+        expect do
+          post url, headers: auth_header(user), params: coupon_invalid_params
+        end.to_not change(Coupon, :count)
+      end
+
+      it 'returns error message' do
+        post url, headers: auth_header(user), params: coupon_invalid_params
+        expect(body_json['errors']['fields']).to have_key('name')
+      end
+
+      it 'returns unprocessable_entity status' do
+        post url, headers: auth_header(user), params: coupon_invalid_params
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
 end
